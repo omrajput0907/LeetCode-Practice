caa51c75a8bf036c964852e8faed49d9798fb91a
omrajput0907 <rajputom91069@gmail.com>
2026-06-17 11:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (63.21%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int count=0,ans=0;
+        for(int i=0;i<matrix.length-1;i++){
+            for(int j=0;j<matrix[0].length-1;j++){
+                if(matrix[i][j]!=matrix[i+1][j+1]){
+                    return false;
+                }
+            }
+            ans++;
+        }
+        return true;
+    }
+
+}
