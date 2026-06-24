33def19f76a3787aac925330800e48272f4f889a
omrajput0907 <rajputom91069@gmail.com>
2026-06-24 10:38:57 +0530

    Time: 1 ms (98.47%) | Memory: 47.5 MB (79.88%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean row=false;
+        boolean col=false;
+        // for(j=0;j<matrix[0].length;j++){
+        //     if(matrix[0][j]==0){
+        //         row=true;
+        //     }
+        // }
+        // for(i=matrix.length-1;i>=0;i--){
+        //     if(matrix[i][0]==0){
+        //         col=true;
+        //     }
+        // }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                    if(i==0){
+                        row=true;
+                    }
+                    if(j==0){
+                        col=true;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[i].length;j++){
+                if(matrix[0][j]==0|| matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(col){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
+        }
+        
+        if(row){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[0][j]=0;
+            }
+        }
+    }
+}
