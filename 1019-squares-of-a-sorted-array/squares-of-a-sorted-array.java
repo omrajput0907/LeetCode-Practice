660e6b5c1c800d5821720728247e6b7e97748a23
omrajput0907 <rajputom91069@gmail.com>
2026-06-29 12:28:31 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (64.28%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,17 +1,31 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
-        int[] m = new int[n];
-        int i = 0, j = n - 1, k = n - 1;
-        while (i <= j) {
-            if (nums[i] * nums[i] >= nums[j] * nums[j]) {
-                m[k--] = nums[i] * nums[i];
-                i++;
-            } else if (nums[i] * nums[i] <= nums[j] * nums[j]) {
-                m[k--] = nums[j] * nums[j];
-                j--;
+        int[] ans = new int[n];
+        int j = 0;
+        while (j < n && nums[j] < 0) {
+            j++;
+        }
+        int i = j - 1;
+        int k = 0;
+        while (i >= 0 && j < n) {
+            if (nums[i] * nums[i] <= nums[j] * nums[j]) {
+                ans[k++] = nums[i] * nums[i];
+                i--;
+            } else {
+                ans[k++] = nums[j] * nums[j];
+                j++;
             }
         }
-        return m;
+        while (i >= 0) {
+            ans[k++] = nums[i] * nums[i];
+            i--;
+        }
+        while (j < n) {
+            ans[k++] = nums[j] * nums[j];
+            j++;
+        }
+
+        return ans;
     }
 }
