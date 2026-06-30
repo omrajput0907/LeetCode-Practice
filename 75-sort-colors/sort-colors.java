5f71ddba12fd32bb57e442978624448efc9a7b2d
omrajput0907 <rajputom91069@gmail.com>
2026-06-30 10:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (59.77%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,37 +1,37 @@
 class Solution {
+   
     public void sortColors(int[] nums) {
-        // space(n);
-        // int[] color=new int[3];
-        // int n=nums.length;
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==0){
-        //         color[0]++;
-        //     }else if(nums[i]==1){
-        //         color[1]++;
-        //     }else{
-        //         color[2]++;
-        //     }
-        // }
-        // int i=0
-        // while(i<color.length){
-        //     if()
-        // }
-
+        int i=0,j=0,z=nums.length-1;
+        while(j<=z){
+            if(nums[j]==0){
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                i++;
+                j++;
+                
+            }else if(nums[j]==1){
+                j++;
+            }else if(nums[j]==2){
+                int temp=nums[j];
+                nums[j]=nums[z];
+                nums[z]=temp;
+                z--;                
+            }
+        }
 
 
         // space(1);
-
-
-        int i=0;
-        while(i<nums.length){
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]<=nums[j]){
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
-            }
-            i++;           
-        }
+        // int i=0;
+        // while(i<nums.length){
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[i]<=nums[j]){
+        //             int temp=nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+        //         }
+        //     }
+        //     i++;           
+        // }
     }
 }
